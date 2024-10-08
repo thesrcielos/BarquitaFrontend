9ee37b55ada92c9a831d09607727dcecb9cf328c
thesrcielos <diegomadi75@gmail.com>
2024-10-08 13:12:23 -0500

    se paso a React las gráficas, el check de las tareas y mostrar su informacion

---
+++ src/insights.js
@@ -0,0 +1,284 @@
+import React, { useEffect, useRef, useState} from 'react';
+import {
+    getHistogram,
+    getAverageByPriority,
+    getTotalTimeSpentByDifficulty,
+    getFinishedTasksByTime
+} from './connectionBackend.js'
+import './insights.css'
+import { 
+    Chart, 
+    CategoryScale, 
+    LinearScale, 
+    BarElement, 
+    LineElement, 
+    PointElement,
+    ArcElement, 
+    PieController, 
+    LineController,
+    BarController,
+    Title, 
+    Tooltip, 
+    Legend
+} from 'chart.js';
+
+// Registra todos los componentes que se van a usar
+Chart.register(CategoryScale, LinearScale, BarElement, ArcElement, LineElement, PointElement, PieController, LineController, BarController, Title, Tooltip, Legend);
+
+var numberTasksByDifficult;
+var averageTasksByPriority;
+var timeSpentByDifficult;
+var finishedTasksByTime;
+
+const Insights = () =>{
+    const [myChart, setMyChart] = useState(null);
+    const chart = useRef(null);
+    const[selectChart, setSelectChart] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            numberTasksByDifficult = await getHistogram();
+            averageTasksByPriority = await getAverageByPriority();
+            timeSpentByDifficult = await getTotalTimeSpentByDifficulty();
+            finishedTasksByTime = await getFinishedTasksByTime();
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+      
+        fetchData();
+      }, []);
+
+    const drawChart = (e) =>{
+        e.preventDefault();
+        if(myChart !== null){ 
+            myChart.destroy();
+        };
+        if(selectChart === 'Tareas por dificultad'){
+            createChartTasksByDifficult();
+        }else if(selectChart === 'Tareas por prioridad'){
+            createChartTasksByPriority();
+        }else if(selectChart === 'Tiempo total tareas'){
+            createChartTimeSpentByTasksDifficult();
+        }else if(selectChart === 'Tareas finalizadas por tiempo'){
+            createChartFinishedTasksByTime();
+        }
+    }
+
+    const handleChange = (e) => {
+        let selectedOption = e.target.value;
+        setSelectChart(selectedOption);
+    }
+
+    const createChartTasksByDifficult = ()=>{
+        let labels = [];
+        let dataHistogram = [];
+        Object.keys(numberTasksByDifficult).forEach(key => {
+            labels.push(key);
+            dataHistogram.push(numberTasksByDifficult[key]);
+        });
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'Tareas por Dificultad',
+                data: dataHistogram,
+                backgroundColor:[
+                    'rgba(239, 236, 20, 0.2)',
+                    'rgba(40, 85, 236, 0.2)',
+                    'rgba(223, 48, 48, 0.2)',
+                ],
+                borderWidth: 1
+            }]
+        };
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    x: {
+                        title: {
+                            display: true,        
+                            text: 'Dificultad', 
+                            font: {
+                                size: 16           
+                            }
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,        // Mostrar etiqueta del eje Y
+                            text: 'Número de Tareas', // Texto para el eje Y
+                            font: {
+                                size: 16           // Tamaño de la fuente del texto
+                            }
+                        },
+                        beginAtZero: true          // Empezar desde 0 en el eje Y
+                    }
+                } // Permitir más flexibilidad en las proporciones
+            }
+        }
+        setMyChart(new Chart(chart.current, config));
+    }
+    
+    //Crea un gráfico del promedio de las tareas por su prioridad basado en la dificultad
+    const createChartTasksByPriority = () => {
+        let labels = [];
+        let dataPriority = [];
+    
+        Object.keys(averageTasksByPriority).forEach(key => {
+            labels.push(key);
+            dataPriority.push(Math.ceil(averageTasksByPriority[key]));
+        });
+    
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'Promedio Tareas por Prioridad',
+                data: dataPriority,
+                backgroundColor:[
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderWidth: 1
+            }]
+        };
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    x: {
+                        title: {
+                            display: true,        
+                            text: 'Prioridad', 
+                            font: {
+                                size: 16           
+                            }
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,        // Mostrar etiqueta del eje Y
+                            text: 'Número de Tareas Promedio', // Texto para el eje Y
+                            font: {
+                                size: 16           // Tamaño de la fuente del texto
+                            }
+                        },
+                        beginAtZero: true          // Empezar desde 0 en el eje Y
+                    }
+                } // Permitir más flexibilidad en las proporciones
+            }
+        };
+        setMyChart(new Chart(chart.current, config));
+    }
+    
+    const createChartTimeSpentByTasksDifficult = ()=>{
+        let labels = [];
+        let dataTime = [];
+    
+        Object.keys(timeSpentByDifficult).forEach(key => {
+            labels.push(key);
+            dataTime.push(timeSpentByDifficult[key]);
+        });
+    
+        const data = {
+            labels: labels,
+            datasets: [{
+              label: 'Tiempo total invertido por tareas realizadas',
+              data: dataTime,
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+              ],
+              hoverOffset: 4,
+              borderWidth: 1
+            }]
+          };
+        const config = {
+            type: 'pie',
+            data: data,
+            options: { 
+                maintainAspectRatio: false,
+            }
+        }
+        setMyChart(new Chart(chart.current, config));
+    }
+    
+    const createChartFinishedTasksByTime = () => {
+        let labels = [];
+        let dataTime = [];
+    
+        Object.keys(finishedTasksByTime).forEach(key => {
+            labels.push(key);
+            dataTime.push(finishedTasksByTime[key]);
+        });
+    
+        const data = {
+            labels: labels,
+            datasets: [{
+              label: 'Tareas Finalizadas por Tiempo',
+              data: dataTime,
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1
+            }]
+          };
+        const config = {
+            type: 'line',
+            data: data,
+            options:{
+                scales: {
+                    x: {
+                        title: {
+                            display: true,        
+                            text: 'Tiempo(horas)', 
+                            font: {
+                                size: 16           
+                            }
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,        // Mostrar etiqueta del eje Y
+                            text: 'Número de Tareas', // Texto para el eje Y
+                            font: {
+                                size: 16           // Tamaño de la fuente del texto
+                            }
+                        }          
+                    }
+                }
+            } 
+          };
+        
+        setMyChart(new Chart(chart.current, config));
+    }
+
+    return (
+        <div>
+            <div className="container-chart-choose">
+            <form id="form-chart" onSubmit={drawChart}>
+            <select name="selected-chart" id="select-chart" value={selectChart} 
+            onChange={handleChange} required>
+                <option value="" disabled>Seleccionar</option>
+                <option value="Tareas por dificultad"> Tareas por dificultad</option>
+                <option value="Tareas finalizadas por tiempo">Tareas finalizadas por tiempo</option>
+                <option value="Tiempo total tareas">Tiempo total por tareas realizadas.</option>
+                <option value="Tareas por prioridad">Promedio tareas por prioridad</option>
+            </select>
+            <input type="submit" className="generate-button" value="Generar"/>
+            </form>
+        </div>
+        <div>
+            <canvas id="myChart" ref={chart}></canvas>
+        </div>
+      </div>
+    );
+}
+
+export default Insights;
+
---
+++ src/tasks.js
@@ -0,0 +1,412 @@
+import React, { useEffect, useState} from 'react';
+import "./App.css";
+import {
+  getAllTasksByState,
+  addTask,
+  deleteTask,
+  updateTask,
+  updateTaskState,
+} from './connectionBackend.js';
+
+
+const difficultyLevels = ['alta', 'media', 'baja'];
+const priorityLevels = ['1', '2', '3', '4', '5'];
+
+const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+  const [tasksCompleted, setTasksCompleted] = useState([]);
+  const [isAddTaskVisible, setIsAddTaskVisible] = useState(false);
+  const [infoTaskId, setInfoTaskId] = useState(null);
+  const [taskInfoId, setTaskInfoId] = useState(null);
+  const [menuVisible, setMenuVisible] = useState(null);
+  const [isTasksNotCompletedVisible, setIsTasksNotCompletedVisible] = useState(true);
+  const [isTasksCompletedVisible, setIsTasksCompletedVisible] = useState(true);
+
+  const toggleMenu = (id) => {
+    setMenuVisible(menuVisible === id ? null : id);
+  }
+
+  const changeTaskNotCompletedVisibility = () => {
+    setIsTasksNotCompletedVisible(!isTasksNotCompletedVisible);
+  }
+
+  const handleChangeStateTask = async (changeTask) => {
+    let taskId = changeTask.id;
+    await updateTaskState(taskId);
+
+    let initialState= changeTask.state;
+    changeTask.state = !initialState;
+
+    if(!initialState){
+      setTasks(tasks.filter(task => task.id !== taskId));
+      setTasksCompleted(prevTasks => [...prevTasks, changeTask]);
+    }else{
+      setTasksCompleted(tasksCompleted.filter(task => task.id !== taskId));
+      setTasks(prevTasks => [...prevTasks, changeTask]);
+    }
+  }
+  const closeTaskInfo = () => {
+    setTaskInfoId(null);
+  }
+  const changeTaskCompletedVisibility = () => {
+    setIsTasksCompletedVisible(!isTasksCompletedVisible);
+  }
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const incompleteTasks = await getAllTasksByState(false);
+      const completedTasks = await getAllTasksByState(true);
+      setTasks(incompleteTasks);
+      setTasksCompleted(completedTasks);
+    };
+
+    fetchTasks();
+  }, []);
+
+  const closeEditForm = () => {
+    setInfoTaskId(null);
+  }
+  
+  const getTaskById = (id) =>{
+    if(id === null) return null;
+    let task = tasks.find(task => task.id === id);
+    if(task !== undefined) return task;
+    return tasksCompleted.find(task => task.id === id);
+  }
+  
+  const handleAddTask = async (newTask) => {
+    await addTask(newTask);
+    setTasks([...tasks, newTask]);
+  };
+
+  const handleDeleteTask = async (taskId, isCompleted) => {
+    await deleteTask(taskId);
+    if (isCompleted) {
+      setTasksCompleted(tasksCompleted.filter(task => task.id !== taskId));
+    } else {
+      setTasks(tasks.filter(task => task.id !== taskId));
+    }
+    toggleMenu(null);
+  };
+
+  const handleUpdateTask = async (updatedTask) => {
+    await updateTask(updatedTask);
+    if (updatedTask.state) {
+      setTasksCompleted(prevTasks => prevTasks.map(task => 
+        task.id === updatedTask.id ? updatedTask : task
+      ));
+    } else {
+      setTasks(prevTasks => prevTasks.map(task => 
+        task.id === updatedTask.id ? updatedTask : task
+      ));
+    }
+  };
+
+  const showAddTaskForm = () => {
+    setIsAddTaskVisible(true);
+  };
+
+  const hideAddTaskForm = () => {
+    setIsAddTaskVisible(false);
+  };
+
+  const TaskList = ({ tasks, onDelete}) => {
+    return (
+      <div className="for-complete">
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const Task = ({ task, onDelete}) => {
+    const handleEdit = () => {
+      setInfoTaskId(task.id);
+    };
+
+    return (
+      <div className={`task-container`}>
+        <button className={`menu-btn`} onClick={() => toggleMenu(task.id)}>⋮</button>
+        <div className={`task-info`} onClick={() => setTaskInfoId(task.id)}>
+          <p className={`task-name`}>{task.name}</p>
+          <p className={`task-date`}>{dateFormat(task.deadline)}</p>
+          <p className={`task-priority`}>Prioridad {task.priority}</p>
+          <p className={`task-difficulty`}>{task.difficulty}</p>
+        </div>
+        {menuVisible === task.id && (
+        <div className={`dropdown-content`}>
+          <button className={`edit-btn`} onClick={() => handleEdit()}>✏️ Editar</button>
+          <button className={`delete-btn`} onClick={()=>onDelete(task.id)}>🗑️ Eliminar</button>
+          <label className={`checkbox-button`}>
+            <input type="checkbox" className={`myCheckbox`} checked={task.state}
+            name="myCheckbox" value="no" onChange={() => handleChangeStateTask(task)}/>
+            <span>Completada</span>
+          </label>
+        </div>
+        )}
+      </div>
+    );
+  
+  };
+  
+  const CompletedTaskList = ({ tasks, onDelete, onUpdate }) => {
+    return (
+      <div className="complete">
+        {tasks.map(task => (
+          <Task
+            key={task.id}
+            task={task}
+            onDelete={onDelete}
+            onUpdate={onUpdate}
+          />
+        ))}
+      </div>
+    );
+  };
+  
+  return (
+    <div className="app-container">
+      <div className="header">
+        <h1>Mis tareas</h1>
+        <div className="buttons">
+          <button className="order-by-priority-button">Ordenar por prioridad 🔝</button>
+          <button className="order-by-difficulty-button">Ordenar por dificultad 👀</button>
+          <button className="order-by-date-button">Filtrar por fecha 📆</button>
+          <button className="add-task-button" onClick={showAddTaskForm}>Agregar tarea ➕</button>
+        </div>
+      </div>
+      {isAddTaskVisible && (
+        <AddTaskForm onAddTask={handleAddTask} onClose={hideAddTaskForm} />
+      )}
+      <div className="tasks-not-completed">
+        <div className="header-not-completed">
+          <button className="hide-button-not-complete" onClick={changeTaskNotCompletedVisibility}>🔻</button>
+          <h2 className="title-task">Por completar</h2>
+        </div>
+        { isTasksNotCompletedVisible && (
+        <TaskList
+          tasks={tasks}
+          onDelete={handleDeleteTask}
+        />
+        )}
+      </div>
+      <div className="tasks-completed">
+        <div className="header-completed">
+          <button className="hide-button-complete" onClick={changeTaskCompletedVisibility}>🔻</button>
+          <h2 className="title-task">Completadas</h2>
+        </div>
+      { isTasksCompletedVisible && (
+        <CompletedTaskList
+          className="container-tasks-completed"
+          tasks={tasksCompleted}
+          onDelete={handleDeleteTask}
+          onUpdate={handleUpdateTask}
+        />
+      )}
+      </div>
+      {/* Aquí puedes incluir contenedores adicionales como edit-info-container, add-task-info-container, etc. */}
+      {taskInfoId !==null && (
+        <TaskInfo
+          task={getTaskById(taskInfoId)}
+          onClose={closeTaskInfo}
+        />
+      )}
+      <div className="date-filter-container">
+        <button className="close-date-filter">✖️</button>
+        <h2>Seleccionar Fecha</h2>
+        <label htmlFor="filter-date">Fecha:</label>
+        <input type="date" id="filter-date" />
+        <button id="filter-by-date-button">Filtrar</button>
+      </div>
+      <div className="difficulty-filter-container">
+        <button className="close-difficulty-filter">✖️</button>
+        <h2>Seleccionar Dificultad</h2>
+        <select id="filter-difficulty">
+          <option value="" disabled>Seleccionar dificultad</option>
+          {difficultyLevels.map(level => (
+            <option key={level} value={level.charAt(0).toUpperCase() + level.slice(1)}>{level.charAt(0).toUpperCase() + level.slice(1)}</option>
+          ))}
+        </select>
+        <button id="apply-difficulty-filter">Aplicar filtro</button>
+      </div>
+      <div className="priority-filter-container">
+        <button className="close-priority-filter">✖️</button>
+        <h2>Seleccionar Prioridad</h2>
+        <select id="filter-priority">
+          <option value="" disabled selected>Seleccionar prioridad</option>
+          {priorityLevels.map(p => (
+            <option key={p} value={p}>{p}</option>
+          ))}
+        </select>
+        <button id="apply-priority-filter">Aplicar filtro</button>
+      </div>
+      {infoTaskId !== null && (
+      <div className='edit-info-container'>       
+          <AddEditForm
+            onEditTask={handleUpdateTask}
+            onClose={closeEditForm}
+            task={getTaskById(infoTaskId)}
+          />
+      </div>
+      )}
+    </div>
+  );
+};
+const TaskForm = ({onSubmit, onClose, onChange, taskData, title}) => {
+  return (<div className='add-task-info-container'>
+      <form className="task-form" onSubmit={onSubmit}>
+        <button className="close-add-task" onClick={onClose}>✖️</button>
+        <h2>{title}</h2>
+        <label htmlFor='name'>Nombre</label>
+        <input
+          className="task-input"
+          type="text"
+          name="name"
+          placeholder="Nombre"
+          value={taskData.name}
+          onChange={onChange}
+          required
+        />
+        <label htmlFor='description'>Descripción</label>
+        <textarea
+          className="task-textarea"
+          name="description"
+          placeholder="Descripción"
+          value={taskData.description}
+          onChange={onChange}
+          required
+        />
+        <label htmlFor='deadline'>Fecha Límite</label>
+        <input
+          className="task-datetime"
+          type="datetime-local"
+          name="deadline"
+          value={taskData.deadline}
+          onChange={onChange}
+          required
+        />
+        <label htmlFor='difficulty'>Dificultad</label>
+        <select className="task-difficulty" name="difficulty" 
+        value={taskData.difficulty} onChange={onChange} required>
+          <option value="">Dificultad</option>
+          {difficultyLevels.map(level => (
+            <option key={level} value={level.toUpperCase()}>{level.toUpperCase()}</option>
+          ))}
+        </select>
+        <label htmlFor='priority'>Prioridad</label>
+        <select className="task-priority" name="priority" 
+        value={taskData.priority} onChange={onChange} required>
+          <option value="">Prioridad</option>
+          {priorityLevels.map(p => (
+            <option key={p} value={p}>{p}</option>
+          ))}
+        </select>
+        <label htmlFor='estimatedTime'>Tiempo Estimado</label>
+        <input name='estimatedTime' type='number' value={taskData.estimatedTime}
+        min='1' max='100' onChange={onChange} required/>
+        <button className="submit-task-button" type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
+const AddEditForm = ({ onEditTask, onClose, task}) => {
+  const [taskData, setTaskData] = useState({
+    id: task.id,
+    name: task.name,
+    description: task.description,
+    deadline: task.deadline,
+    difficulty: task.difficulty,
+    priority: task.priority,
+    state: task.state,
+    estimatedTime: task.estimatedTime
+  });
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onEditTask({ ...taskData});
+    onClose();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData(prevData => ({ ...prevData, [name]: value }));
+  };
+
+  return (<TaskForm 
+            onSubmit={handleSubmit}
+            onClose={onClose}
+            onChange={handleChange}
+            taskData={taskData}
+            title={"Editar Tarea"}
+        />);
+
+}
+const AddTaskForm = ({ onAddTask, onClose }) => {
+  const [taskData, setTaskData] = useState({
+    name: '',
+    description: '',
+    deadline: '',
+    difficulty: '',
+    priority: '',
+    estimatedTime: 1
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData(prevData => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddTask({ ...taskData, state: false });
+    onClose();
+  };
+
+  return (
+    <TaskForm 
+      onSubmit={handleSubmit}
+      onClose={onClose}
+      onChange={handleChange}
+      taskData={taskData}
+      title={"Crear Tarea"}
+    />
+  );
+};
+
+const TaskInfo = ({task, onClose}) => {
+  return (
+    <div className='visualize-task-info-container'>
+      <div className="visualize-task-info">
+        <button className="close-visualize" onClick={onClose}>x</button>
+        <p>Nombre: {task.name}</p>
+        <p>Descripción: {task.description}</p>
+        <p>Fecha límite: {dateFormat(task.deadline)}</p>
+        <p>Dificultad: {task.difficulty}</p>
+        <p>Prioridad: {task.priority}</p>
+        <p>Tiempo Estimado: {task.estimatedTime}</p>
+      </div>
+    </div>
+  );
+}
+function dateFormat(date){
+  let format = new Date(date);
+  return `${format.getDate()}/${format.getMonth()+1}/${format.getFullYear()} 
+          - ${hoursFormat(format.getHours().toString())}:${minuteFormat(format.getMinutes().toString())}`;
+}
+
+// Función para formatear los minutos a dos dígitos
+function minuteFormat(minutes){
+  return minutes.length > 1 ? minutes : "0"+minutes;
+}
+
+// Función para formatear las horas a dos dígitos
+function hoursFormat(hours){
+  return hours.length > 1 ? hours : "0"+hours;
+}
+
+export default Tasks;
